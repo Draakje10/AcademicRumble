08896540baf3006ed0992fd3a7a1eb930cc6255e
JVP11292021 <jessyvanpolanen@gmail.com>
2023-04-04 09:08:48 +0200

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.academicrumble {
     requires javafx.fxml;
 
     requires com.almasb.fxgl.all;
+    requires annotations;
 
     opens com.example.academicrumble to javafx.fxml;
     exports com.example.academicrumble;
