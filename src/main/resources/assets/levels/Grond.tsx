a78e2dc86b16fed6810334eb15258270384ecd5c
Draakje10 <58084526+Draakje10@users.noreply.github.com>
2023-04-04 11:33:26 +0200

    Fixed the tilemap

--- src/main/resources/assets/levels/Grond.tsx
+++ src/main/resources/assets/levels/Grond.tsx
@@ -1,4 +1,4 @@
 <xml version="1.0" encoding="UTF-8">
 <tileset version="1.2" tiledversion="1.2.3" name="Grond" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="Grond.png" width="32" height="32"/>
+ <image source="/Grond.png" width="32" height="32"/>
 </tileset>
